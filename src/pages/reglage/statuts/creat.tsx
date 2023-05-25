333beee342ab7ae55533e942ed83203410dba84f
Mamadou Diagne <genova@dofbi.com>
2023-05-25 09:57:03 +0000

    ✨ feat (pages): reglage/status

---
+++ src/pages/reglage/statuts/creat.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { IResourceComponentsProps } from "@refinedev/core";
+import { Create, useForm } from "@refinedev/antd";
+import { Form, Input } from "antd";
+
+export const StatutCreate: React.FC<IResourceComponentsProps> = () => {
+    const { formProps, saveButtonProps } = useForm();
+
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Nom Statut"
+                    name={["nom_statut"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
---
+++ src/pages/reglage/statuts/list.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { IResourceComponentsProps, BaseRecord } from "@refinedev/core";
+import { useTable, List, EditButton } from "@refinedev/antd";
+import { Table, Space } from "antd";
+
+export const StatutList: React.FC<IResourceComponentsProps> = () => {
+    const { tableProps } = useTable({
+        syncWithLocation: true,
+    });
+
+    return (
+        <List>
+            <Table {...tableProps} rowKey="id">
+                <Table.Column dataIndex="id" title="Id" />
+                <Table.Column dataIndex="nom_statut" title="Nom Statut" />
+                <Table.Column
+                    title="Actions"
+                    dataIndex="actions"
+                    render={(_, record: BaseRecord) => (
+                        <Space>
+                            <EditButton
+                                hideText
+                                size="small"
+                                recordItemId={record.id}
+                            />
+                        </Space>
+                    )}
+                />
+            </Table>
+        </List>
+    );
+};
